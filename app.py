66e7f419f3162990a755daf6614b95ed87d0cde4
Josh Cook <joshuacook0023@gmail.com>
2021-07-08 19:46:25 -0400

    feat: expandable sections with additional notes and recipes

--- app.py
+++ app.py
@@ -3,16 +3,17 @@
 """A Streamlit application for visualizing and analyzing my coffee consumption."""
 
 from copy import deepcopy
-from datetime import datetime
+from datetime import datetime, timedelta
+from typing import Optional
 
 import altair as alt
-import numpy as np
 import pandas as pd
 import streamlit as st
 
 import coffee_counter as cc
+import informational_text
 
-DEV = True
+DEV = False
 
 #### ---- App info ---- ####
 
@@ -22,20 +23,7 @@ st.header("A visualization and analysis of my coffee-consumption.")
 
 more_info_expander = st.beta_expander("More info")
 with more_info_expander:
-    st.markdown(
-        """
-    **Background:** I have a coffee subscription to [Black Rifle Coffee Company](http://blackriflecoffee.com).
-    I currently order two bags of coffee every two weeks.
-
-    **The problem:** Recently, I have noticed that I often finish my two bags before the arrival of my next order.
-
-    **The solution:** To solve this problem, I have collected data on my coffee consumption, specifically, tracking the lifetimes' of coffee bags and when I brew a cup of coffee with each.
-    I will then use this data to identify the optimal subscription frequency.
-
-    **Implementation:** I have created a [web API](https://a7a9ck.deta.dev/docs) ([source](https://github.com/jhrcook/coffee-counter-api); using [FastAPI](https://fastapi.tiangolo.com) and [Deta](https://www.deta.sh)) to store the data and allow simple and fast access from anywhere.
-    I then created a [SwiftBar](https://swiftbar.app) plugin to allow me to [register a cup of coffee from my computers menu bar](https://github.com/jhrcook/SwiftBar-Plugins/blob/master/coffee-tracker.1h.py) and am working on an iOS application to let me register a cup from my phone.
-    """
-    )
+    st.markdown(informational_text.more_info())
 
 st.markdown(" ")  # A bit of space between 'More info' and 'Refresh' button.
 
@@ -57,7 +45,6 @@ if st.button("Refresh Data"):
     _data = load_data()
 
 coffee_use_data["date"] = [d.date() for d in coffee_use_data["datetime"]]
-
 coffee_use_data["finish"] = [
     d if d is not None else datetime.today().date() for d in coffee_use_data["finish"]
 ]
@@ -117,33 +104,59 @@ cups_per_day_with_rolling_avg = (cups_over_time_plot + cups_rolling_avg).propert
 )
 chart_placeholder.altair_chart(cups_per_day_with_rolling_avg, use_container_width=True)
 
+last_week_avg = coffee_uses_per_day[
+    coffee_uses_per_day.date >= (datetime.today().date() - timedelta(days=7))
+].n_cups.mean()
+
+
+st.markdown(
+    f"""
+The plot above shows the number of cups of coffee I've had since I began tracking.
+Over the entire {coffee_uses_per_day.shape[0]} days, I have averaged {coffee_uses_per_day["n_cups"].mean():0.2f} cups of coffee per day.
+Over the last week, I have averaged {last_week_avg:0.2f} cups.
+"""
+)
+
 #### ---- Historgrams of uses per day and per bag ---- ####
 
 col1, col2 = st.beta_columns(2)
 
 
-def altair_histogram(df: pd.DataFrame) -> alt.Chart:
+def altair_histogram(
+    df: pd.DataFrame,
+    x_title: Optional[str] = "number of cups",
+    y_title: Optional[str] = "count",
+    title: str = "",
+    bin_tooltip: bool = True,
+) -> alt.Chart:
     return (
         alt.Chart(df)
         .mark_bar()
         .encode(
-            alt.X("n_cups:Q", bin=True),
-            y="count()",
+            x=alt.X("n_cups:Q", bin=True, title=x_title),
+            y=alt.Y("count()", title=y_title),
             tooltip=[
                 alt.Tooltip("count():N", title="count"),
-                alt.Tooltip("n_cups:Q", bin=True, title="num. cups"),
+                alt.Tooltip("n_cups:N", bin=bin_tooltip, title="num. cups"),
             ],
         )
+        .properties(title=title)
         .interactive()
     )
 
 
 with col1:
-    cups_per_day_histogram = altair_histogram(coffee_uses_per_day)
+    cups_per_day_histogram = altair_histogram(
+        coffee_uses_per_day,
+        x_title="number of cups",
+        title="Distribution of cups of coffee per day",
+        bin_tooltip=False,
+    )
     st.altair_chart(cups_per_day_histogram)
 
 cups_per_bag_df = (
-    coffee_use_data.groupby("bag_id")[["use_id"]]
+    coffee_use_data.query("active == 0")
+    .groupby("bag_id")[["use_id"]]
     .count()
     .reset_index(drop=False)
     .rename(columns={"use_id": "n_cups"})
@@ -151,8 +164,25 @@ cups_per_bag_df = (
 
 with col2:
     # st.dataframe(cups_per_bag_df)
-    cups_per_bag_histogram = altair_histogram(cups_per_bag_df)
+    cups_per_bag_histogram = altair_histogram(
+        cups_per_bag_df,
+        x_title="number of cups",
+        title="Distribution of cups of coffee per coffee bag",
+    )
     st.altair_chart(cups_per_bag_histogram)
 
 
 #### ---- Horizontal bars showing lifetimes of bags ---- ####
+
+
+#### ---- Notes on coffee use and other relevant comments ---- ####
+
+st.markdown("---")
+
+notes_expander = st.beta_expander("Notes", expanded=DEV)
+with notes_expander:
+    st.markdown(informational_text.notes())
+
+recipes_expander = st.beta_expander("Recipes", expanded=DEV)
+with recipes_expander:
+    st.markdown(informational_text.recipes())
